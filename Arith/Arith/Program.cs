07ee50c9d40305eaf2e412fe9dac582292454dbb
PallaviVangari <vangaripallavi27@gmail.com>
2019-02-01 10:40:09 -0800

    addition

---
+++ Arith/Arith/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9, b = 7;
+            int res = a + b;
+            Console.WriteLine(res);
+
+        }
+    }
+}
